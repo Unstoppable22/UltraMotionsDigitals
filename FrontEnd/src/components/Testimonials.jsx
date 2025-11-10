38e96534eb25a39a5124ad624b3cc8ff0a45c806
Unstoppable22 <tboybade@gmail.com>
2025-11-09 23:59:31 -1200

    done

--- FrontEnd/src/components/Testimonials.jsx
+++ FrontEnd/src/components/Testimonials.jsx
@@ -17,7 +17,7 @@ export default function Testimonials() {
             alt="" 
             className="rounded-full w-15 h-15 " 
           />
-          <h1 className="text-[15px] text-left  lg:py-5">
+          <h1 className="text-[15px] text-center  lg:py-5">
             Ultra Motions Digital has been a game-changer for our business. Their unmatched creativity, attention to detail, and dedication helped shape a digital presence that truly reflects our brand’s vision. We witnessed incredible growth in both engagement and sales. Their professionalism and passion shine through, and we wholeheartedly recommend them to any company aiming for success and long-term results.
           </h1>
           <h2>- Adekunle Akinola</h2>
@@ -28,7 +28,7 @@ export default function Testimonials() {
             alt=""
             className="rounded-full w-15 h-15 "
           />
-          <h1 className="text-[15px] text-left py-5">
+          <h1 className="text-[15px] text-center py-5">
             Partnering with Ultra Motions Digital was the best decision we made for our brand. Their creativity, precision, and unwavering dedication turned our digital presence into something truly remarkable. We not only saw our engagement soar but also built a stronger connection with our audience. Their work speaks volumes, and we wholeheartedly recommend them to any business striving for excellence.
           </h1>
           <h2>- Oladimeji Alabi</h2>
@@ -39,7 +39,7 @@ export default function Testimonials() {
             alt=""
             className="rounded-full w-15 h-15 "
           />
-          <h1 className="text-[15px] text-left py-5">
+          <h1 className="text-[15px] text-center py-5">
            Working with Ultra Motions Digital delivered Massive results for our brand. Their vision, creative excellence, and unwavering dedication elevated our online presence to new heights. We saw tremendous growth in engagement and conversions. Their commitment to delivering excellence is evident, and we confidently recommend their services to any business looking to achieve lasting success and remarkable impact.
           </h1>
           <h2>- Ogunsanwo Mide</h2>
