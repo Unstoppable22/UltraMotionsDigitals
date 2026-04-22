31740c87879a66b7a88f238cd28c3cc4fbfc855d
Unstoppable22 <tboybade@gmail.com>
2026-04-22 05:02:43 -1200

    pol

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -7,7 +7,7 @@ import Footer from "./components/Footer";
 // Pages
 import Home from "./pages/Home";
 import Faq from "./components/Faq";
-import AboutUs from "./pages/aboutus.jsx";
+import Aboutus from "./components/Aboutus";
 import CategorySectionGroup from "./components/Categorysectiongroup.jsx";
 import Testimonials from "./components/Testimonials";
 import LaunchSection from "./components/Launchsection.jsx";
