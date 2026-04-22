cad889d9b4b9583154ac97ee12606f11d6d1f3ca
Unstoppable22 <tboybade@gmail.com>
2026-04-22 06:21:02 -1200

    pol

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -20,7 +20,7 @@ import Signup from "./components/Signup";
 import Login from "./components/Login";
 
 // Admin Pages - DOUBLE CHECK THESE PATHS FOR CASE SENSITIVITY
-import Adminlogin from "./pages/admin/AdminLugeen.jsx";
+import Adminlogin from "./pages/admin/Adminlogeen.jsx";
 import Admindashboard from "./pages/admin/Admindashboard.jsx";
 
 // Route Protection
