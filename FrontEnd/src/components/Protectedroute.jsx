15cebb7cbb3d8fe8abec35dfe11f9287bee8363f
Unstoppable22 <tboybade@gmail.com>
2026-04-24 03:52:28 -1200

    fixed

--- FrontEnd/src/components/Protectedroute.jsx
+++ FrontEnd/src/components/Protectedroute.jsx
@@ -1,7 +1,8 @@
 import { Navigate } from "react-router-dom";
 
 export default function ProtectedRoute({ children }) {
-  const token = localStorage.getItem("userToken");
+  // Match this to what you used in Login.jsx
+  const token = localStorage.getItem("token");
 
   if (!token) {
     return <Navigate to="/login" replace />;
